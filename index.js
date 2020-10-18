a1a86afb33aa5a2c95756c77d6b312c49823e1fe
djstephenson7 <dan_0502@hotmail.co.uk>
2020-10-18 14:56:41 +0100

    Basic Apollo server

---
+++ index.js
@@ -0,0 +1,40 @@
+const { ApolloServer } = require('apollo-server');
+const gql = require('graphql-tag');
+const mongoose = require('mongoose');
+const { MONGODB } = require('./config');
+const Post = require('./models/Post');
+
+const typeDefs = gql`
+  type Post {
+    id: ID!
+    body: String!
+    createdAt: String!
+    username: String!
+  }
+  type Query {
+    getPosts: [Post]
+  }
+`;
+
+const resolvers = {
+  Query: {
+    async getPosts() {
+      try {
+        const posts = await Post.find();
+        return posts;
+      } catch (error) {
+        throw new Error(error);
+      }
+    },
+  },
+};
+
+const server = new ApolloServer({ typeDefs, resolvers });
+
+mongoose
+  .connect(MONGODB, { useNewUrlParser: true, useUnifiedTopology: true })
+  .then(() => {
+    return server.listen({ port: 5000 });
+  })
+  .then((res) => console.log(`Server running at ${res.url}`))
+  .catch((e) => console.log(`ERROR: ${e}`));

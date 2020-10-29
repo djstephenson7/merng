3fe105c887d9e4086ccf4b3eab2a7f03a3e25a42
djstephenson7 <dan_0502@hotmail.co.uk>
2020-10-29 11:37:41 +0000

    User validation

--- index.js
+++ index.js
@@ -1,33 +1,9 @@
 const { ApolloServer } = require('apollo-server');
-const gql = require('graphql-tag');
+const typeDefs = require('./graphql/typeDefs');
+
 const mongoose = require('mongoose');
 const { MONGODB } = require('./config');
-const Post = require('./models/Post');
-
-const typeDefs = gql`
-  type Post {
-    id: ID!
-    body: String!
-    createdAt: String!
-    username: String!
-  }
-  type Query {
-    getPosts: [Post]
-  }
-`;
-
-const resolvers = {
-  Query: {
-    async getPosts() {
-      try {
-        const posts = await Post.find();
-        return posts;
-      } catch (error) {
-        throw new Error(error);
-      }
-    },
-  },
-};
+const resolvers = require('./graphql/resolvers');
 
 const server = new ApolloServer({ typeDefs, resolvers });
 
